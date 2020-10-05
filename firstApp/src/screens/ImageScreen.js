00707131eed5978fc7060c0ae2cb23b0b78195b7
RaihanEvan <raihan.evan@gmail.com>
2020-10-05 09:51:52 +0600

    ImageScreen Created

---
+++ firstApp/src/screens/ImageScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+
+const ImageScreen = ()=>{
+    return(
+        <View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create(
+    {
+
+    }
+);
+
+export default ImageScreen;
