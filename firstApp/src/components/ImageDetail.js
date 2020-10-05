a66de98b1b636fcfa91f1428e47ca992fd47f538
RaihanEvan <raihan.evan@gmail.com>
2020-10-05 13:50:18 +0600

    components added

--- firstApp/src/components/ImageDetail.js
+++ firstApp/src/components/ImageDetail.js
@@ -2,10 +2,22 @@ import React from 'react';
 import {Text,View,Image} from 'react-native';
 
 const ImageDetail = (props)=>{
-    console.log(props);
+    let ImageSource = undefined;
+    if(props.title=="Beach"){
+        ImageSource=require("../../assets/beach.png");
+    }
+    else if(props.title=="Forest"){
+        ImageSource = require("../../assets/forest.png");
+    }
+    else{
+        ImageSource=require("../../assets/mountain.png");
+    }
     return(
-        <View>
+        <View style={styles.viewStyle}>
             <Text style={styles.textStyle}>{props.title}</Text>
+            <Image 
+            source={ImageSource} style={styles.imageStyle}
+            />
         </View>
     );
 }
@@ -15,6 +27,16 @@ const styles = StyleSheet.create(
         textStyle:{
             fontSize:30,
             color:'red',
+        },
+        imageStyle:{
+            height:100,
+            width:200,
+            margin:5,
+        },
+        viewStyle:{
+            borderColor:'blue',
+            borderWidth:5,
+            margin:5,
         },
     }
 );
