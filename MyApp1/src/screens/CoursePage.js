f53856815bb3e0d9bbe2168fc2078b7dc2a16de1
RaihanEvan <raihan.evan@gmail.com>
2020-10-05 15:55:57 +0600

    Course page added & semester populated

---
+++ MyApp1/src/screens/CoursePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text,View,StyleSheets} from 'react-native';
+
+const CoursePage = ()=>{
+    return(
+        <View></View>
+    );
+};
+const styles = StyleSheets.create(
+    {
+        textViews:{
+
+        }
+    }
+);
