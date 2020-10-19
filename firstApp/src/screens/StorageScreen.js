433379032ffa49d4d1e4de4b84ba73185630886b
RaihanEvan <raihan.evan@gmail.com>
2020-10-19 16:58:13 +0600

    defined async func

--- firstApp/src/screens/StorageScreen.js
+++ firstApp/src/screens/StorageScreen.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import React from 'react-native';
-import {Text, View, Button,AsyncStorage} from 'react-native';
+import { Text, View, Button, AsyncStorage } from 'react-native';
 
-const StorageScreen = ()=>{
-    let func=()=>{
-        let i=0;
-        for(i=0;i<100000000000;i++){}
-        alert("Done");
+const StorageScreen = () => {
+    let func = async () => {
+        await function () {
+            let i = 0;
+            for (i = 0; i < 100000000000; i++) { }
+            alert("Done");
+        };
     };
     func();
-    return(
+    return (
         <View>
             <Text>Storage Screen</Text>
-            <Button title='Get Data' onPress={function(){}}/>
-            <Button title='Remove Data' onPress={function(){}}/>
+            <Button title='Get Data' onPress={function () { }} />
+            <Button title='Remove Data' onPress={function () { }} />
         </View>
     );
 };
