d3b27f1cedee39d5acaac5f8f820dd26c6387ac4
RaihanEvan <raihan.evan@gmail.com>
2020-10-19 16:55:12 +0600

    added sync type function

--- firstApp/src/screens/StorageScreen.js
+++ firstApp/src/screens/StorageScreen.js
@@ -3,6 +3,12 @@ import React from 'react-native';
 import {Text, View, Button,AsyncStorage} from 'react-native';
 
 const StorageScreen = ()=>{
+    let func=()=>{
+        let i=0;
+        for(i=0;i<100000000000;i++){}
+        alert("Done");
+    };
+    func();
     return(
         <View>
             <Text>Storage Screen</Text>
